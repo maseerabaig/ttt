1bdf6cd82165e848bf2341f1a1e97051e3ffa23c
maseerabaig <121236205+maseerabaig@users.noreply.github.com>
2025-03-03 18:28:15 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -4,7 +4,7 @@ const userSchema = new mongoose.Schema({
     userId: { type: String, required: true, unique: true },
     username: { type: String, required: true },
     email: { type: String, required: true, unique: true },
-    profileImage: { type: String } // Local file path of image
+    profileImage: { type: String } 
 });
 
 module.exports = mongoose.model("User", userSchema);
